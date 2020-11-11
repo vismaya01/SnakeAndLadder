5e601a5b14cfe78a8c5d4ede2a6f60cb10f54c30
vismaya01 <kvvismaya98@gmail.com>
2020-11-11 20:28:21 +0530

    Modified: Player position after every die role

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,33 +3,41 @@ import java.util.Random;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int playerPosition = 0;
+		int count = 0;
 		Random random = new Random();
-		int playerRollDie = random.nextInt(6 - 1) + 1;
-		int rollDie = random.nextInt(3);
-		while (playerPosition < 100) {   
-			switch (rollDie) {
-				case 0:
+        	 while (playerPosition < 100) {                  
+        		int playerRollDie = random.nextInt(6 - 1) + 1;
+        		System.out.println(playerRollDie);
+        		count = count + 1;
+ 		 	int rollDie = random.nextInt(3);
+ 		 	System.out.println("roll="+rollDie);
+ 		 	switch (rollDie) {
+ 		   		case 0:
  		   			playerPosition = playerPosition + playerRollDie;
- 					if (playerPosition > 100){
- 		   				playerPosition=playerPosition-playerRollDie;
- 		   			}
-					break;
- 				case 1:
+ 		  			if (playerPosition > 100){
+ 	  					playerPosition=playerPosition-playerRollDie;
+ 	   				}
+ 	  				System.out.println("player position is " +playerPosition);
+ 					break;
+ 		   		case 1:
  					playerPosition = playerPosition - playerRollDie;
-					if (playerPosition <= 0 ) {
+ 					if (playerPosition <= 0 ) {
+ 						System.out.println("player position is 0" );
  						SnakeAndLadder.main(args);
- 		 			}
-					break;
-	 			case 2:
- 					playerPosition = playerPosition;
+		 			}
+ 					System.out.println("player position is " +playerPosition);
  					break;
- 				default:
+ 		   		case 2:
+ 					System.out.println("player position is " +playerPosition);
  					break;
- 		 		}
-		}
-		if (playerPosition == 100) {
-			System.out.println("you won the game");
- 			System.exit(0);
- 		}
+				default:
+ 					break;
+ 	 		}
+ 			if (playerPosition == 100) {
+ 				System.out.println("you won the game");
+ 				System.out.println("the number of time dice rolled is "+count);
+ 				System.exit(0);
+ 			}
+         	}
 	}
 }
