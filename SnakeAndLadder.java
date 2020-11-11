83b1f0f0adacd70ea8ce4cd99c7629cac8e37593
vismaya01 <kvvismaya98@gmail.com>
2020-11-11 20:00:44 +0530

    Added: checks for No Play, Ladder or Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,5 +5,19 @@ public class SnakeAndLadder {
 		int playerPosition = 0;
 		Random random = new Random();
 		int playerRollDie = random.nextInt(6 - 1) + 1;
+		int rollDie = random.nextInt(3);
+		switch (rollDie) {
+			case 0:
+ 		   		playerPosition = playerPosition + playerRollDie;
+ 				break;
+ 			case 1:
+ 				playerPosition = playerPosition - playerRollDie;
+				break;
+ 			case 2:
+ 				playerPosition = playerPosition;
+ 				break;
+ 			default:
+ 				break;
+ 		 	}
 	}
 }
