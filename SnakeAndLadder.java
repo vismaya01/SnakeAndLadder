4bf83fc9869428f6611b4efdcea270120a7989e9
vismaya01 <kvvismaya98@gmail.com>
2020-11-11 20:09:54 +0530

    Added: Player reaches the winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,18 +6,27 @@ public class SnakeAndLadder {
 		Random random = new Random();
 		int playerRollDie = random.nextInt(6 - 1) + 1;
 		int rollDie = random.nextInt(3);
-		switch (rollDie) {
-			case 0:
- 		   		playerPosition = playerPosition + playerRollDie;
- 				break;
- 			case 1:
- 				playerPosition = playerPosition - playerRollDie;
-				break;
- 			case 2:
- 				playerPosition = playerPosition;
- 				break;
- 			default:
- 				break;
- 		 	}
+		while (playerPosition < 100) {   
+			switch (rollDie) {
+				case 0:
+ 		   			playerPosition = playerPosition + playerRollDie;
+ 					break;
+ 				case 1:
+ 					playerPosition = playerPosition - playerRollDie;
+					if (playerPosition <= 0 ) {
+ 						SnakeAndLadder.main(args);
+ 		 			}
+					break;
+	 			case 2:
+ 					playerPosition = playerPosition;
+ 					break;
+ 				default:
+ 					break;
+ 		 		}
+		}
+		if (playerPosition >= 100) {
+			System.out.println("you won the game");
+ 			System.exit(0);
+ 		}
 	}
 }
